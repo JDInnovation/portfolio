1f2e3464d649afab1ebebdee9e70a0aa7682f8dd
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-08-09 21:23:02 +0100

    .

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -62,24 +62,12 @@ export default function Footer(props) {
             </a>
           </motion.div>
           <motion.div className="footer-section footer-contact">
-            <h4>Contato e Localização</h4>
+            <h4>Contato</h4>
             <ul>
               <li><span>Email:</span> <a href="mailto:info@jd-invest-group.com">info@jd-invest-group.com</a></li>
               <li><span>Telefone:</span> <a href="tel:+351123456789">+351 123 456 789</a></li>
               <li><span>Endereço:</span> Ponte de Vagos, Aveiro, Portugal</li>
             </ul>
-          </motion.div>
-          <motion.div className="footer-section footer-map">
-            <h4>Localização</h4>
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3121.96534232632!2d-8.686011584682547!3d40.52821877935039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd237e53a2439af1%3A0x9c29f1b73b40a4c0!2sEstrada%20Principal%20194%2C%203840-216%20Ponte%20de%20Vagos%2C%20Portugal!5e0!3m2!1sen!2sus!4v1625076244312!5m2!1sen!2sus" 
-              width="300" 
-              height="200" 
-              style={{ border: 0 }}
-              allowFullScreen="" 
-              loading="lazy"
-              title="Localização"
-            ></iframe>
           </motion.div>
         </motion.div>
         <motion.p 
