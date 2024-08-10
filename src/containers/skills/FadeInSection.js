58ef1b32e9adfe842c7a6fff7a43d2ac909abaf5
JDInnovation <jdinvestgroup@outlook.com>
2024-08-10 04:34:58 +0100

    kkh

--- src/containers/skills/FadeInSection.js
+++ src/containers/skills/FadeInSection.js
@@ -2,17 +2,15 @@ import React from "react";
 import { useSpring, animated } from "react-spring";
 import { useInView } from "react-intersection-observer";
 
-function FadeInSection({ children, delay = 0, duration = 1000, direction = "left" }) {
-  const [ref, inView, entry] = useInView({
+function FadeInSection({ children, delay = 0, duration = 350 }) {
+  const [ref, inView] = useInView({
     triggerOnce: false, // Permite que a animação aconteça ao entrar e sair
-    threshold: 0.1, // Ajuste conforme necessário
+    threshold: 0.3, // Ajuste conforme necessário
   });
-
-  const fromX = direction === "left" ? "-100px" : "100px"; // Direção de onde o conteúdo entra e sai
 
   const animation = useSpring({
     opacity: inView ? 1 : 0,
-    transform: inView ? "translateX(0) scale(1)" : `translateX(${fromX}) scale(0.8)`,
+    transform: inView ? "translateY(0) scale(1)" : "translateY(7px) scale(0.99)", // De baixo para cima com zoom
     config: { duration },
     delay: inView ? delay : 0,
   });
