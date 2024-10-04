ac53bf36bda092568349887ddbd0384f8cd9fd78
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-10-04 21:03:54 +0100

    .

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import { TooltipProvider } from "react-tooltip";
 import Main from "./containers/Main";
 import "./App.css";
 import "react-tooltip/dist/react-tooltip.css";
-import Hotjar from '@hotjar/browser';
 
-const siteId = 5159711;
-const hotjarVersion = 6;
 
 Hotjar.init(siteId, hotjarVersion);
 function App() {
