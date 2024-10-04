460488cd7e9a40428e982759caae232467a1adde
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-10-04 19:07:36 +0100

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ import { TooltipProvider } from "react-tooltip";
 import Main from "./containers/Main";
 import "./App.css";
 import "react-tooltip/dist/react-tooltip.css";
+import Hotjar from '@hotjar/browser';
 
+const siteId = 5159711;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
   const useCursor = settings.useCustomCursor;
